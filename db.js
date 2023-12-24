8593b06c26e94270d49e5f11218f17b5a6583186
akhilkongonda <akhilkongonda84@gmail.com>
2023-12-25 00:23:14 +0530

    timeinl

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const connection =mysql.createConnection({
     user:'root',
     password:'root1234',
     database:'result_analysis',
-    connectTimeout: 20000,
+    connectTimeout: 100000,
     port:3306
 });
 //checking if connection is succesful or not
